d66d5812cfd42a349e08b35e6b3ee9458d8e8bab
ms-henglu <henglu@microsoft.com>
2021-08-19 16:07:22 +0800

    export config and coverage test for v2.65.0

---
+++ azurerm/internal/acceptance/export.go
@@ -0,0 +1,44 @@
+package acceptance
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/types"
+)
+
+func (td TestData) ExportConfig(t *testing.T, testResource types.TestResource, steps []TestStep) {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	workingDirectory = workingDirectory[:strings.Index(workingDirectory, "internal")]
+	if runtime.GOOS == "windows" {
+		workingDirectory += "export\\"
+	} else {
+		workingDirectory += "export/"
+	}
+	if _, err := os.Stat(workingDirectory); os.IsNotExist(err) {
+		err = os.Mkdir(workingDirectory, 0744)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i, step := range steps {
+		if len(step.Config) > 0 {
+			filename := fmt.Sprintf(workingDirectory+"%v_step_%v.tf", strings.ReplaceAll(t.Name(), "/", "_"), i)
+			d1 := []byte(step.Config)
+			err = ioutil.WriteFile(filename, d1, 0744)
+			if err != nil {
+				t.Fatalf("fail to write file: %v", err)
+			} else {
+				fmt.Printf("config exported to file : %v", filename)
+			}
+		}
+	}
+}
